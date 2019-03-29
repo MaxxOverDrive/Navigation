97b2f4253aa06b83d84a53a30ab98e578bdc91e1
Mattymo86 <mattw@materialflow.com>
2019-03-29 16:56:53 -0700

    Reactive 1

--- index.php
+++ index.php
@@ -4,18 +4,30 @@
     <meta charset="utf-8">
     <title>Responsive Navigation</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link rel="stylesheet" href="CSS/style.css">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
 <body>
-  <div class="container">
-    <div class="row">
-      <div class="col-sm-12 text-center">
-
-
+    <nav>
+      <div class="logo">
+        <h4>The Nav</h4>
       </div>
-     </div>
-   </div>
+
+      <ul class="nav-links">
+        <li><a href="#">Home</a></li>
+        <li><a href="#">About</a></li>
+        <li><a href="#">Work</a></li>
+        <li><a href="#">Projects</a></li>
+      </ul>
+      <div class="burger">
+        <div class="line1"></div>
+        <div class="line2"></div>
+        <div class="line3"></div>
+      </div>
+    </nav>
+
    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+   <script src="JS/app.js"></script>
 </body>
 </html>
